d29cd0268c3504212f48a62a6ca65ad922305d5e
TANAKA-PC\TANAKA <t.y.21.lami@gmail.com>
2017-12-19 17:13:21 +0900

    popup

--- app/src/main/java/com/example/tanaka/outside/MainActivity.java
+++ app/src/main/java/com/example/tanaka/outside/MainActivity.java
@@ -2,12 +2,70 @@ package com.example.tanaka.outside;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener{
+    private Button button;
+    private PopupWindow mPopupWindow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button show_button =findViewById(R.id.show_button);
+
+        Toast.makeText(this, "テスト1", Toast.LENGTH_LONG).show();
+
+        button=(Button)this.findViewById(R.id.show_button);
+        button.setOnClickListener(this);
     }
+    public void onClick(View view){
+        Toast.makeText(this, "2テスト2", Toast.LENGTH_LONG).show();
+        mPopupWindow = new PopupWindow(MainActivity.this);
+
+
+        // レイアウト設定
+        View popupView = getLayoutInflater().inflate(R.layout.popup_layout, null);
+        popupView.findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPopupWindow.isShowing()) {
+                    mPopupWindow.dismiss();
+                }
+            }
+        });
+        mPopupWindow.setContentView(popupView);
+
+        // 背景設定
+        mPopupWindow.setBackgroundDrawable(getResources().getDrawable(R.drawable.popup_background));
+
+        // タップ時に他のViewでキャッチされないための設定
+        mPopupWindow.setOutsideTouchable(true);
+        mPopupWindow.setFocusable(true);
+
+        // 表示サイズの設定 今回は幅300dp
+        float width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics());
+        mPopupWindow.setWindowLayoutMode((int) width, WindowManager.LayoutParams.WRAP_CONTENT);
+        mPopupWindow.setWidth((int) width);
+        mPopupWindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+
+        // 画面中央に表示
+        mPopupWindow.showAtLocation(findViewById(R.id.show_button), Gravity.CENTER, 0, 0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mPopupWindow != null && mPopupWindow.isShowing()) {
+            mPopupWindow.dismiss();
+        }
+        super.onDestroy();
+    }
+
 }
